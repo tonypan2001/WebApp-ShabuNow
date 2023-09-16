527c60788f1c7f4132ab0c0b59796012b082d1b1
tonypan2001 <Panzaza9543@gmail.com>
2023-09-16 08:21:20 +0700

    installing tailwindcss for nuxtjs(frontend)

--- ShabuNow-nuxt/app.vue
+++ ShabuNow-nuxt/app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <h1 class="text-3xl font-bold underline text-center">
+      Hello, World!
+      Hello, Tailwindcss!
+    </h1>
+    <h2 class="text-xl font-bold text-center">
+      Tailwindcss is work!
+    </h2>
   </div>
 </template>
