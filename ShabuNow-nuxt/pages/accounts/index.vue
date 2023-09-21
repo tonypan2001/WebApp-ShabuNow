17c7a9d17f4471d0da6eed989d0ef68b29e77344
thanetjin <thanet.jin@ku.th>
2023-09-21 21:22:02 +0700

    admin home and admin create menu is done

--- ShabuNow-nuxt/pages/accounts/index.vue
+++ ShabuNow-nuxt/pages/accounts/index.vue
@@ -1,100 +1,88 @@
 <template>
-    <MainContainer>
-        <HeaderContainer>
-            <HeaderText>
-                บัญชีของคุณ
-            </HeaderText>
-        </HeaderContainer>
-        <hr>
-        <ContentContainer>
-            <!-- ข้อมูลส่วนตัว -->
-            <div class="w-2/3 p-4 text-xl font-light border rounded-xl mt-8" v-for="user in users">
+  <MainContainer>
+    <HeaderContainer>
+      <HeaderText> บัญชีของคุณ </HeaderText>
+    </HeaderContainer>
+    <hr />
+    <ContentContainer>
+      <!-- ข้อมูลส่วนตัว -->
+      <div
+        class="w-2/3 p-4 text-xl font-light border rounded-xl mt-8"
+        v-for="user in users"
+      >
+        <h1 class="text-2xl font-semibold text-red-600">ข้อมูลส่วนตัว</h1>
 
-                <h1 class="text-2xl font-semibold text-red-600">ข้อมูลส่วนตัว</h1>
+        <p class="mt-4">
+          <span class="font-medium"> ชื่อลูกค้า : </span>
+          {{ user.name }}
+        </p>
+        <p class="mt-4">
+          <span class="font-medium"> วันที่สมัครสมาชิก : </span>
+          {{ user.mem_date }}
+        </p>
+        <p class="mt-4">
+          <span class="font-medium"> แต้มสะสม : </span>
+          {{ user.points }}
+        </p>
+      </div>
 
-                <p class="mt-4">
-                    <span class="font-medium">
-                        ชื่อลูกค้า :
-                    </span> 
-                    {{ user.name }}
-                </p>
-                <p class="mt-4">
-                    <span class="font-medium">
-                        วันที่สมัครสมาชิก :
-                    </span> 
-                    {{ user.mem_date }}
-                </p>
-                <p class="mt-4">
-                    <span class="font-medium">
-                        แต้มสะสม : 
-                    </span> 
-                    {{ user.points }}
-                </p>
-            </div>
-
-            <!-- เปลี่ยนรหัสผ่าน -->
-            <div class="w-2/3 p-4 text-xl font-light border rounded-xl mt-8" v-for="user in users">
-
-                <h1 class="text-2xl font-semibold text-red-600">เปลี่ยนรหัสผ่าน</h1>
-                <p class="mt-4">
-                    <span class="font-medium">
-                        อีเมลล์ :
-                    </span> 
-                    {{ user.email }}
-                </p>
-                <!-- password input field -->
-                <!-- <label class="relative block mt-4">
+      <!-- เปลี่ยนรหัสผ่าน -->
+      <div
+        class="w-2/3 p-4 text-xl font-light border rounded-xl mt-8"
+        v-for="user in users"
+      >
+        <h1 class="text-2xl font-semibold text-red-600">เปลี่ยนรหัสผ่าน</h1>
+        <p class="mt-4">
+          <span class="font-medium"> อีเมลล์ : </span>
+          {{ user.email }}
+        </p>
+        <!-- password input field -->
+        <!-- <label class="relative block mt-4">
                     <span class="absolute inset-y-0 left-0 flex items-center pl-2">
                         <i class="bi bi-lock-fill text-2xl mr-2"></i>
                     </span>
                     <input class="placeholder:text-slate-400 block bg-white border border-slate-300 rounded-md w-full py-3 pl-9 pr-3 shadow-sm focus:outline-none focus:border-red-600 focus:ring-red-600 focus:ring-1 sm:text-base" placeholder="รหัสผ่านใหม่" type="password" name="password"/>
                 </label> -->
-                <InputField placeholder="รหัสผ่านใหม่" type="password" name="password">
-                    <template v-slot:icon>
-                        <i class="bi bi-lock-fill text-2xl mr-2"></i> 
-                    </template>
-                </InputField>
-                <!-- confirm password input field -->
-                <InputField placeholder="รหัสผ่านใหม่อีกครั้ง" type="password" name="confirm-password"/>
-            </div>
-        </ContentContainer>
-        <hr>
-        <!-- History table -->
-        <Table :datas="tableData" :headers="tableHeaders" class="mt-8">
-            <template v-slot:title>
-                ประวัติการใช้บริการ
-            </template>
-
-        </Table>
-    </MainContainer>
+        <InputField placeholder="รหัสผ่านใหม่" type="password" name="password">
+          <template v-slot:icon>
+            <i class="bi bi-lock-fill text-2xl mr-2"></i>
+          </template>
+        </InputField>
+        <!-- confirm password input field -->
+        <InputField
+          placeholder="รหัสผ่านใหม่อีกครั้ง"
+          type="password"
+          name="confirm-password"
+        />
+      </div>
+    </ContentContainer>
+    <hr />
+    <!-- History table -->
+    <Table :datas="tableData" :headers="tableHeaders" class="mt-8">
+      <template v-slot:title> ประวัติการใช้บริการ </template>
+    </Table>
+  </MainContainer>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            // customers or users detail
-            users: [
-                {
-                    name: 'Prompan Uechanwech',
-                    mem_date: '11/10/2023',
-                    points: '125',
-                    email: 'username01@example.com',
-                    password: '',
-                }
-            ],
-            // add some datas to table
-            tableHeaders: [
-                'No.',
-                'Price',
-                'Date',
-                'Used Points',
-            ],
-            // add table headers
-            tableData: [
-                ['1', '1750', '11/12/2023', '150'],
-            ],
-        }
-    },
-}
+  data() {
+    return {
+      // customers or users detail
+      users: [
+        {
+          name: "Prompan Uechanwech",
+          mem_date: "11/10/2023",
+          points: "125",
+          email: "username01@example.com",
+          password: "",
+        },
+      ],
+      // add some datas to table
+      tableHeaders: ["No.", "Price", "Date", "Used Points"],
+      // add table headers
+      tableData: [["1", "1750", "11/12/2023", "150"]],
+    };
+  },
+};
 </script>
---
+++ ShabuNow-nuxt/pages/admins/index.vue
@@ -0,0 +1,99 @@
+<template>
+  <MainContainer>
+    <HeaderContainer>
+      <HeaderText> โปรดเลือกเมนูสุดคุ้ม </HeaderText>
+
+      <!-- category dropdown button -->
+      <ButtonDropdown title="หมวดหมู่อาหาร" :items="categories" class="mb-4" />
+      <!-- end -->
+    </HeaderContainer>
+    <hr />
+
+    <ContentContainer>
+      <!-- menu container -->
+      <!-- Category Tag -->
+      <HeaderContainer class="w-full">
+        <div
+          class="text-2xl font-medium m-4 py-1.5 px-4 rounded-xl bg-red-600 text-white"
+        >
+          <h1>ประเภท : อาหารคาว</h1>
+        </div>
+        <!-- manage category  -->
+        <Button>
+          <slot name="button">จัดการหมวดหมู่อาหาร</slot>
+        </Button>
+      </HeaderContainer>
+      <GridContainer>
+        <!-- menu item card -->
+        <MenuItemCard v-for="food in foods" :imageUrl="food.imageUrl">
+          <template v-slot:title>
+            <!-- สลัดผักรวมมิตร -->
+            {{ food.title }}
+          </template>
+          <template v-slot:price>
+            <!-- ราคา ฿55 บาท -->
+            {{ food.price }}
+          </template>
+          <template v-slot:button> เลือกเมนูนี้ </template>
+        </MenuItemCard>
+        <!-- end -->
+      </GridContainer>
+      <!-- end menu container -->
+    </ContentContainer>
+  </MainContainer>
+</template>
+
+<script>
+export default {
+  // just some data
+  data() {
+    return {
+      foods: [
+        {
+          imageUrl:
+            "https://img.freepik.com/free-photo/delicious-vietnamese-food-including-pho-ga-noodles-spring-rolls-white-wall_181624-34158.jpg?w=1480&t=st=1694914784~exp=1694915384~hmac=28ce6696273496ac0f862aacccb0f3fcf9bb4b466ea118dc34c9bc3aaac883b0",
+          title: "Salad",
+          price: "฿75",
+        },
+        {
+          imageUrl:
+            "https://img.freepik.com/free-photo/fried-pork-with-garlic-pepper-served-with-rice-fried-egg_1150-27374.jpg?w=1480&t=st=1694917028~exp=1694917628~hmac=817a4137204b388d2caebdb1832e665b25d9137da1a062d8ac87461377696e95",
+          title: "Fried Pork Rice",
+          price: "฿85",
+        },
+        {
+          imageUrl:
+            "https://img.freepik.com/free-photo/fried-chicken-parts-fusilli-white-plate_114579-72409.jpg?w=1480&t=st=1694919254~exp=1694919854~hmac=42c4ec7371dcd64d7efcd793238b259b333273cb049d67c36d4a4b26a29294a7",
+          title: "Pasta and Chicken",
+          price: "฿125",
+        },
+        {
+          imageUrl:
+            "https://img.freepik.com/free-photo/stir-fried-instant-noodles-with-seafood-variety-vegetable_1150-27317.jpg?w=740&t=st=1694933851~exp=1694934451~hmac=a4574fd32652c87e0203d95bf152a23675fcca588220cc5edd22b30321a3eef4",
+          title: "Spaghetti",
+          price: "฿145",
+        },
+      ],
+      // dropdown button
+      categories: [
+        {
+          title: "Food",
+          link: "#",
+        },
+        {
+          title: "Drink",
+          link: "#",
+        },
+        {
+          title: "Ice Cream",
+          link: "#",
+        },
+        {
+          title: "Hot",
+          link: "#",
+        },
+      ],
+    };
+  },
+};
+</script>
