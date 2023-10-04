2f490e25553d1ab6f0ff90f54504287380e30fe1
Thanadon Mahaparn <thanadon.mhp@gmail.com>
2023-10-04 21:50:35 +0700

    New Factories.

--- ShabuNow/database/seeders/DatabaseSeeder.php
+++ ShabuNow/database/seeders/DatabaseSeeder.php
@@ -12,11 +12,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(UserSeeder::class);
+        $this->call(TableSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(MenuSeeder::class);
+        $this->call(OrderSeeder::class);
     }
 }
